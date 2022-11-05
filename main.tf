d8931ac79dbfc9bf28be8afa069c05e3a0af6c29
Simon Andrews <simon@hubblehq.com>
2022-11-05 22:35:07 +0000

    feat: Enable public IP on database

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "google_sql_database_instance" "instance" {
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled    = false
+      ipv4_enabled    = true
       private_network = google_compute_network.private_network.id
     }
   }
