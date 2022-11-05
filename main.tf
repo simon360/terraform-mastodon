83f4069dca07ba95bd7e1b9a677889084c643e5e
Simon Andrews <simon@hubblehq.com>
2022-11-05 16:31:59 +0000

    feat: Remove test resources

--- main.tf
+++ main.tf
@@ -15,30 +15,3 @@ terraform {
     prefix = "terraform/state"
   }
 }
-
-resource "google_project_service" "compute_api" {
-  service = "compute.googleapis.com"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
