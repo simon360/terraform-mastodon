9274ba82c5b839d355c9b49b8e8f9b39fd1c42a6
Simon Andrews <simon@hubblehq.com>
2022-11-05 15:40:47 +0000

    chore: Add editorconfig

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 variable "GCLOUD_PROJECT" {
-    type = string
-    description = "The project to terraform in"
+  type        = string
+  description = "The project to terraform in"
 }
 
 provider "google" {
   project = GCLOUD_PROJECT
-  region = "europe-west1"
+  region  = "europe-west1"
 }
