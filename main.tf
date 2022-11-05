bbb6e2b5a43118034fb63501654e7a4e2f62bc41
Simon Andrews <simon@hubblehq.com>
2022-11-05 17:37:58 +0000

    feat: terraform a cloud run instance

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ variable "GCLOUD_PROJECT" {
   description = "The project to terraform in"
 }
 
+variable "RUN_IMAGE" {
+  type        = string
+  description = "The image tag to use in Cloud Run"
+}
+
 provider "google" {
   project = var.GCLOUD_PROJECT
   region  = "europe-west1"
@@ -18,4 +23,26 @@ terraform {
 
 resource "google_project_service" "build_api" {
   service = "cloudbuild.googleapis.com"
+}
+
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "sadl-mastodon-srv"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = var.RUN_IMAGE
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
 }
