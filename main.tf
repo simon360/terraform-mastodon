4c89b0319a128dd0e65e697d3cd9fee226a9908c
Simon Andrews <simon@hubblehq.com>
2022-11-05 15:48:20 +0000

    feat: create a basic compute instance

--- main.tf
+++ main.tf
@@ -7,3 +7,26 @@ provider "google" {
   project = GCLOUD_PROJECT
   region  = "europe-west1"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
